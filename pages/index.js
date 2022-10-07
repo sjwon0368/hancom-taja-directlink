ab1ea3f5541295aeb9c9ca78ad2c3503f5f81878
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-10-07 16:07:56 +0900

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -16,22 +16,22 @@ export default function Home() {
           아래 <code>링크</code> 중 하나를 클릭해 한컴타자의 여러가지 <code>게임과 프로그램</code>을 즐기세요!
         </p>
         <p className="description">
-          <a target='_popup' href='https://tt-exam-c.malangmalang.com/typing/WebContent/?exercise=typing-writing'>한컴타자</a>
+          <a target='_blank' href='https://tt-exam-c.malangmalang.com/typing/WebContent/?exercise=typing-writing'>한컴타자</a>
         </p>
         <p className="description">
-          <a target='_popup' href='https://tt-coin-c.malangmalang.com/coinpile_sample/WebContent/?channel=1'>동전쌓기</a>
+          <a target='_blank' href='https://tt-coin-c.malangmalang.com/coinpile_sample/WebContent/?channel=1'>동전쌓기</a>
         </p>
         <p className="description">
-          <a target='_popup' href='https://tt-mole-c.malangmalang.com/mole/WebContent/'>두더지잡기</a>
+          <a target='_blank' href='https://tt-mole-c.malangmalang.com/mole/WebContent/'>두더지잡기</a>
         </p>
         <p className="description">
-          <a target='_popup' href='https://tt-block-c.malangmalang.com/Flip01/WebContent/'>판뒤집기</a>
+          <a target='_blank' href='https://tt-block-c.malangmalang.com/Flip01/WebContent/'>판뒤집기</a>
         </p>
         <p className="description">
-          <code style=''>안내! 현재 '말랑말랑 산성비'는 대전교육정보원에 의해 차단되었기 때문에 집에서 개인 컴퓨터로 접속해주세요!</code>
+          <code style='color=red;'>안내! 현재 말랑말랑 산성비는 대전교육정보원에 의해 차단되었기 때문에 집에서 개인 컴퓨터로 접속해주세요!</code>
         </p><br/>
         <p className="description">
-          <a target='_popup' disabled href='https://rain.malangmalang.com/'>말랑말랑 산성비</a>
+          <a target='_blank' disabled href='https://rain.malangmalang.com/'>말랑말랑 산성비</a>
         </p>
       </main>
 

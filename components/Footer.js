8f2bfc5684469bf92bba53191f07cc501c0209df
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-10-16 21:41:10 +0900

    Initial Commit

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
+      <footer className={styles.footer}><br/><br/>
         한컴타자 다이렉트링크는&nbsp;<a target='_blank' href='https://github.com'>GitHub</a>와&nbsp;<a href='https://app.netlify.com/' target='_blank'>Netlify</a>의 도움으로 만들어졌습니다.
-        Version 2.4.0 Build 86747598634736659781 <code>master</code>/<code>45fec8d</code>
-      </footer>
+        <br/>Version 2.4.0 Build 86747598634736659781 <br/><code>master</code>/<code>45fec8d</code>
+      </footer><br/><br/>
     </>
   )
 }

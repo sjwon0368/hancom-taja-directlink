ad1887177c7cbe5d4782493fc56e74226890f096
sjwon0368 <82024430+sjwon0368@users.noreply.github.com>
2022-12-23 12:26:40 +0900

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export default function Home() {
       <main>
         <Header title="한컴타자 다이렉트링크" />
         <p className="description">
-          아래 <code>링크</code> 중 하나를 클릭해 한컴타자의 여러가지 <code>게임과 프로그램</code>을 즐기세요!
+          한컴타자가 새로워졌어요! 아래의 <code>링크</code>를 클릭해 <code>새로운 게임과 타자연습</code>을 즐기세요!
         </p>
         <p className="description">
           <a target='_blank' href='https://tt.hancomtyping.com/?t=&r='>한컴타자</a>
         </p>
         <p className="description">
-          <code className='warnCode'>안내: 현재 한컴타자 동전쌓기는 말랑말랑플렛폼 서비스 업데이트에 의해 서버가 일시적으로 중단되었기 때문에 접속이 불가능 할 수 있습니다.</code>
+          <code className='warnCode'>안내: 최신 버전의 한컴타자는 온라인 Unity플레이어로 만들어졌기 떄문에 불러오는 데 다소 시간이 걸릴 수 있습니다.</code>
         </p><br/>
         <p className="description">
           <a target='_blank' href='https://tt-coin-c.malangmalang.com/coinpile_sample/WebContent/?channel=1'>동전쌓기 채널 1</a>
